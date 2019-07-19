3ea21da19bc934c2a2eb94e605e67357eb08a01a
sunhuaiqing <410329486@qq.com>
2019-07-19 10:47:01 +0800

    修复XXXX1.1123123

--- src/main/java/com/example/demo/BocIntegralQuery.java
+++ src/main/java/com/example/demo/BocIntegralQuery.java
@@ -21,15 +21,24 @@ public class BocIntegralQuery {
         return baseList;
     }
 
+
+
     @RequestMapping()
     public @ResponseBody String bocMoked(HttpServletRequest request) throws Exception {
+        System.out.println("request.getQueryString():"+request.getQueryString());
+        //System.out.println("request.changeSessionId():"+request.changeSessionId());
+        System.out.println("request.getParameter(\"data\")():"+request.getParameter("data"));
+        System.out.println("request.getPathInfo():"+request.getPathInfo());
+        System.out.println("request.getParameterMap():"+request.getParameterMap());
+
+
         String resp = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
                 "<data>\n" +
                 "    <accessToken>a5g7jxAtoqjOXU7/TvlupKIhAXwzvqqFeOD/oOvReTkzRB5XLOLW1pwRqQBUmW4za6Cz9ah9NHwnrPcTyORC2SZOZrxjkG6D0Ag2TZ4gAfqEPuvvabOwZfLJ5eoW66sOf/n2g9OJz7CIgruSGIQZJ60uZ55hMvUUyQMZo0M01dU=</accessToken>\n" +
                 "    <pointValue>530000</pointValue>\n" +
                 "    <pointType>1</pointType>\n" +
                 "    <bizCode>0000</bizCode>\n" +
-                "    <bizDesc>中行积分查询成功Mock</bizDesc>\n" +
+                "    <bizDesc>中行积分查询成功Mock1</bizDesc>\n" +
                 "</data>\n";
         return  resp;
     }
