f0c8e87fd681827af00551e447bd4a61064b57a6
sunhuaiqing <410329486@qq.com>
2020-01-19 11:41:37 +0800

    2020diyici112312123

--- src/main/java/com/example/demo/BocValidVerifCode.java
+++ src/main/java/com/example/demo/BocValidVerifCode.java
@@ -29,7 +29,7 @@ public class BocValidVerifCode {
                     "    <accessToken>F2hSkr2f8ng0YdjH1/ybkmKoGJ+iSl+27/d4bBbG42LtQyUFXswSW8G7i9vkGc9Xi92n01oXiATdjsEBM4GpRMfsRVQIadNrbTTjICLObRoVa1rMm15Khf2nVX/Xf42fp9AwLwdQkUR2Cu/q2u1hQVZRJkgytyVbQv6WD/M8+bM=</accessToken>\n" +
                     "  <userId>d211HWB7+dF+1CyNPJIZryl5Ybk/8u1ha/UENjGm8ARnZlDsbUvHDlr3j7G0DaRSd31RJRhlRyUzATsoZD4IiVQN3GptzQgY5snRQXNyxB2AvJhxs/PJhOo96I45PVFxCBghYb3B1dQM5ivE1NNQaAydwLtA8L9teo84zV3lKDY=</userId>\n" +
                     "    <bizCode>0000</bizCode>\n" +
-                    "    <bizDesc>中行短信验证码校验成功Mocker</bizDesc>\n" +
+                    "    <bizDesc>中行短信验证码校验成功Mocker12</bizDesc>\n" +
                     "</data>";
 
         return  resp;
